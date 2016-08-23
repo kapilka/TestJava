ee603f0898fe7deb0becf02da64481746c27a47e
Khurana <KhuranaK@ES.AD.ADP.com>
2016-08-23 17:27:38 +0530

    initial commit

---
+++ Java/src/Inheritance/Son.java
@@ -0,0 +1,24 @@
+package Inheritance;
+
+public class Son extends GrandFather {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Son s = new Son();
+		s.Country();
+		s.city();
+		s.activities();
+		System.out.println(s.i);
+		
+			
+	}
+
+	public void activities()
+	{
+		System.out.println("Activities...");
+
+		System.out.println("Testing GIT.");
+
+	}
+}
